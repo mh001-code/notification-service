07decbee6bb2dedc168cca55a427f0d73c732e35
Márcio Henrique <marcio.henrique09@gmail.com>
2026-05-21 11:28:16 -0300

    chore: initial notification-service setup

--- src/main/java/notification_service/NotificationServiceApplication.java
+++ src/main/java/notification_service/NotificationServiceApplication.java
@@ -1,9 +1,11 @@
-package notification_service;
+package com.orderprocessing.notification.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
--- src/test/java/notification_service/NotificationServiceApplicationTests.java
+++ src/test/java/notification_service/NotificationServiceApplicationTests.java
@@ -1,9 +1,7 @@
-package notification_service;
+package com.orderprocessing.notification.service;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class NotificationServiceApplicationTests {
 
 	@Test
